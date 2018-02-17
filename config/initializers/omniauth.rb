104a14254b042570093606b4b8a1742b10934b94
rin <souma_rin7th@yahoo.co.jp>
2018-02-18 05:09:14 +0900

    issues #4 フォロー権限を削除

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,12 +1,12 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :mastodon, scope: 'read write follow', credentials: lambda { |domain, callback_url|
+  provider :mastodon, scope: 'read write', credentials: lambda { |domain, callback_url|
     Rails.logger.info "Requested credentials for #{domain} with callback URL #{callback_url}"
 
     existing = MastodonClient.find_by(domain: domain)
     return [existing.client_id, existing.client_secret] unless existing.nil?
 
     client = Mastodon::REST::Client.new(base_url: "https://#{domain}")
-    app = client.create_app('OffDoon', callback_url, 'read write follow')
+    app = client.create_app('OffDoon', callback_url, 'read write')
 
     MastodonClient.create!(domain: domain, client_id: app.client_id, client_secret: app.client_secret)
 
