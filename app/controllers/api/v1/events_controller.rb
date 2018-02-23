f34697a2560b45789104e1fd7a4dd44161e39138
rinSouma <souma_rin7th@yahoo.co.jp>
2018-02-23 16:23:18 +0900

    token未指定の時にis nullで検索されてhitしていたので修正

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -4,7 +4,7 @@ module Api
       def index
         begin
           #ヘッダに設定されたトークンからユーザ情報を取得
-          @user = User.find_by(token: request.headers[:HTTP_TOKEN])
+          @user = User.find_by(token: request.headers[:HTTP_TOKEN]) if request.headers[:HTTP_TOKEN].present?
           @events = Event.search_api(params, @user)
           render json: @events.to_json(:include => [:members, :comments])
         rescue=>e
