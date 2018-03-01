de9c6578119a3c57b8437f4eb0e6bac924faf8ef
rin <souma_rin7th@yahoo.co.jp>
2018-03-02 02:01:38 +0900

    参加上限を超えて参加できる不具合を修正

--- app/controllers/member_controller.rb
+++ app/controllers/member_controller.rb
@@ -1,6 +1,11 @@
 class MemberController < ApplicationController
   def create
-    
+    if params[:member][:limit] then
+      if params[:member][:limit].to_i < params[:member][:count].to_i + 1 then
+        redirect_to home_path(params[:member][:event_id])
+        return
+      end
+    end
     
     member = Member.find_or_initialize_by(event_id: params[:member][:event_id], uid: params[:member][:uid])
     member.update_attributes(member_params)
@@ -8,9 +13,9 @@ class MemberController < ApplicationController
   end
   
   def destroy
-    comment = Comment.find(params[:id])
-    comment.destroy
-    redirect_to home_path(comment.event_id)
+    member = Member.find(params[:id])
+    member.destroy
+    redirect_to home_path(member.event_id)
   end
   
   private
